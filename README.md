c0f4cfcba40515a21f7fd8fd908ac1eb0f4ed64e
RevengeofBlob <mnbakerboy1@gmail.com>
2025-05-02 00:13:46 -0400

    Get actual image from website

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Discord bot that uses the Chronicles of Arcane card database to display cards in
 
 ## Set up
 pip install discord.py
+pip install BeautifulSoup4
+pip install selenium
 
 In your discord bot, turn "MESSAGE CONTENT INTENT" on
--- server.py
+++ server.py
@@ -1,8 +1,20 @@
 import os
-import urllib.request
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import requests
+import time
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome(options=chrome_options)
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -24,14 +36,34 @@ async def on_ready():
     global web_html
 
     try:
-        response = urllib.request.url(url)
-        web_html = response.read().decode('UTF-8')
-        print(web_html)
+        driver.get(url)
     except Exception as e:
         print(f"Exception Occurd: {e}")
 
-    # with open("test.html", "w") as file:
-    #     file.write(web_html)
+    old_height = driver.execute_script('return document.body.scrollHeight')
+    new_height = None
+    while True:
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(2)
+        new_height = driver.execute_script('return document.body.scrollHeight')
+
+        if new_height == old_height:
+            break
+        old_height = new_height
+
+    web_html = driver.page_source
+
+    driver.quit()
+
+    soup = BeautifulSoup(web_html, "html.parser")
+    images = []
+    images = soup.find_all("img")
+    img_url = images[20]["src"]
+    print(images[20]["src"])
+
+    img_data = requests.get(img_url).content
+    with open("image.jpg", "wb") as f:
+        f.write(img_data)
 
 @bot.command("getcard")
 async def get_card(ctx, card_wanted):
